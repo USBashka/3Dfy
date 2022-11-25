b7edd0a5a5dd27b0577fa6c08479825234f0f0e6
USBashka <usbashka@gmail.com>
2022-11-25 23:53:38 +0400

    update 3Dfy.py

--- 3Dfy.py
+++ 3Dfy.py
@@ -5,6 +5,10 @@ from PIL import Image
 import torch
 from transformers import GLPNFeatureExtractor, GLPNForDepthEstimation
 
+import numpy as np
+import open3d as o3d
+
+
 
 feature_extractor = GLPNFeatureExtractor.from_pretrained("vinvino02/glpn-nyu")
 model = GLPNForDepthEstimation.from_pretrained("vinvino02/glpn-nyu")
@@ -37,10 +41,29 @@ output = output[pad:-pad, pad:-pad]
 image = image.crop((pad, pad, image.width - pad, image.height - pad))
 
 # visualize the prediction
-fig, ax = plt.subplots(1, 2)
-ax[0].imshow(image)
-ax[0].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-ax[1].imshow(output, cmap='gray')
-ax[1].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-plt.tight_layout()
-plt.pause(115)
+# fig, ax = plt.subplots(1, 2)
+# ax[0].imshow(image)
+# ax[0].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+# ax[1].imshow(output, cmap='gray')
+# ax[1].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+# plt.tight_layout()
+# plt.pause(115)
+
+width, height = image.size
+
+depth_image = (output * 255 / np.max(output)).astype('uint8')
+image = np.array(image)
+
+# create rgbd image
+depth_o3d = o3d.geometry.Image(depth_image)
+image_o3d = o3d.geometry.Image(image)
+rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(image_o3d, depth_o3d, convert_rgb_to_intensity=False)
+
+# camera settings
+camera_intrinsic = o3d.camera.PinholeCameraIntrinsic()
+camera_intrinsic.set_intrinsics(width, height, 500, 500, width/2, height/2)
+
+# create point cloud
+pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, camera_intrinsic)
+
+o3d.visualization.draw_geometries([pcd])
